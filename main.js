24e90f9468984f6e6e7b404aabf4f2c10f8d817f
onom <onom1013@gmail.com>
2022-12-10 01:21:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   fill(255,0,0)
+  fill(0)
   background(0);
 
 }
 
 function draw() {
-  background(0,35);
+  // background(0,35);
 
 
-  strokeWeight(8)
+  strokeWeight(2)
   stroke(255,0,0)
   // line(mouseX,mouseY,pmouseX,pmouseY);
   // line(width-mouseX,mouseY,width-pmouseX,pmouseY);
 
-  line(mouseX,mouseY,width-mouseX,mouseY);
+  // line(mouseX,mouseY,width-mouseX,mouseY);
 
 
-  noStroke()
-  ellipse(mouseX,mouseY,80)
-  ellipse(width-mouseX,mouseY,80)
+  // noStroke()
+  let s = random(80,200)
+  ellipse(mouseX,mouseY,s)
+  ellipse(width-mouseX,mouseY,s)
 
 }

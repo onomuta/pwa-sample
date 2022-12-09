5a81f396378c32f95cc864efd4595a99241a46e5
onom <onom1013@gmail.com>
2022-12-10 01:13:45 +0900

    hoge

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  fill(255,0,0,50)
+  background(0);
+
 }
 
 function draw() {
-  background(220,5);
-  rect(mouseX,mouseY,10,10)
+  background(0,35);
+
+
+  strokeWeight(8)
+  stroke(255,0,0)
+  line(mouseX,mouseY,pmouseX,pmouseY);
+  line(width-mouseX,mouseY,width-pmouseX,pmouseY);
+
+
+  noStroke()
+  ellipse(mouseX,mouseY,80)
+  ellipse(width-mouseX,mouseY,80)
+
 }

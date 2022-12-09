4997b137776cb45ef6de112f07e43f4a944228dc
onom <onom1013@gmail.com>
2022-12-10 00:48:49 +0900

    hoge

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
-  background(220,5);
-  rect(mouseX,mouseY,10,10)
+  // background(220,5);
+  // rect(mouseX,mouseY,10,10)
+
+  background(0);
+	push();
+	{
+		rotateY(frameCount / 60.0);
+		push();
+		translate(-width/6.0, 0, 0)
+		rotateY(frameCount / 30.0);
+		sphere(height/4);
+		pop();
+		push();
+		translate(width/6.0, 0, 0);
+		rotateY(frameCount / 30.0);
+		box(height/3);
+		pop();
+	}
+	pop();
 }
